fddadbfc29c989eeb7a8b92d9ca3eb53a613544f
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 05:45:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,9 @@ end
 
 def take_a_number (katz_deli, name)
   
+  array = katz_deli << name
+  puts "Welcome, #{name}. You are number #{array.size()} in line."
+
 end
 
 def now_serving (katz_deli)
