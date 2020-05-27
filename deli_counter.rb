9edc1b0f6892c17e18680447f359ef372242f4c5
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-27 05:38:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+katz_deli = []
+
+def line (other_deli)
+  
+  index = 0
+  
+  if (other_deli.size == 0)
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    other_deli.each do |person|
+      katz_deli[index] = "#{index + 1}. #{person}"
+      index += 1
+    end
+    puts "#{katz_deli}"
+  end
+  
+end
+
+def take_a_number (katz_deli, name)
+  
+end
+
+def now_serving (katz_deli)
+  
+end
